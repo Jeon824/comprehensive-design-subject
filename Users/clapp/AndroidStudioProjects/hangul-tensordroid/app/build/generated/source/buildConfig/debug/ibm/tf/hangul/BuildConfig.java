40868f35bcdd6f8dd3a781888400fb369c453efd
clappingmin <clappingmin@naver.com>
2020-06-12 15:44:58 +0900

    day 1

---
+++ Users/clapp/AndroidStudioProjects/hangul-tensordroid/app/build/generated/source/buildConfig/debug/ibm/tf/hangul/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ibm.tf.hangul;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "hangultensordroid.ibm.org.tensorflowhangulrecognition";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ Users/clapp/AndroidStudioProjects/hangul-tensordroid/app/src/main/java/ibm/tf/hangul/MainActivity2.java
@@ -0,0 +1,35 @@
+package ibm.tf.hangul;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+            // 화면 전환 - 인텐트 날리기 (startActivity)
+            //     1. 다음 넘어갈 화면을 준비한다 (layout xml, java)
+            //    2. AndroidManifest.xml 에 Activity 를 등록한다
+            //    3. Intent 객체를 만들어서 startActivity 한다
+
+            Button b = (Button)findViewById(R.id.button);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(
+                            getApplicationContext(), // 현재 화면의 제어권자
+                            MainActivity.class); // 다음 넘어갈 클래스 지정
+                    startActivity(intent); // 다음 화면으로 넘어간다
+                }
+            });
+        } // end onCreate()
+
+    }
